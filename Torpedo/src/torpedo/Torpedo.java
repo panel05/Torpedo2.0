058bc6c338a56b776f845348c0ab339a71e7f4a8
panel05 <pantlbalazs@gmail.com>
2022-05-19 14:47:07 +0200

    hajoalap

--- Torpedo/src/torpedo/Torpedo.java
+++ Torpedo/src/torpedo/Torpedo.java
@@ -2,7 +2,14 @@ package torpedo;
 public class Torpedo {
 
     public static void main(String[] args) {
-       
+        
     }
+
+    public static String tesztLoves(int poz) {
+        
+        return null;
+        
+    }
+    
     
 }
